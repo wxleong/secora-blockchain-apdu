104730442a765585c9b2f71bfe10ceae8c15bbcb
wenxin.leong <wenxin.leong@infineon.com>
2022-12-06 16:51:41 +0800

    Cherry-pick files from github.com/Infineon/BlockchainSecurity2Go-Android
    Signed-off-by: wenxin.leong <wenxin.leong@infineon.com>

---
+++ src/main/java/com/github/wxleong/ByteUtils.java
@@ -0,0 +1,56 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2022 Infineon Technologies AG
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE
+ *
+ */
+/**
+ * This file was copied from https://github.com/Infineon/BlockchainSecurity2Go-Android
+ * (sha1: 0d27d53) and modified accordingly.
+ *
+ * MIT License
+ * Copyright (c) 2019 Coinfinity GmbH
+ */
+package com.github.wxleong;
+
+/**
+ * Some helper utils to work with byte arrays.
+ */
+public class ByteUtils {
+
+    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+    /**
+     * returns hexadecimal string representation of a byte array.
+     *
+     * @param bytes byte array
+     * @return hex string
+     */
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+}
